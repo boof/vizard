d22be563ea455c8899c430c5634c444471dfefac
Florian Aßmann <flazy@fork.de>
2011-07-17 19:32:50 +0200

    Completed input and output functions for noscript filter.

--- source/filter/noscript.js
+++ source/filter/noscript.js
@@ -2,14 +2,19 @@
 (function(filter) {
 
 	// script types could be evaluated
-	var SCRIPT_TYPEs  = /type=["'](?:text|application)\/(?:x-)?(?:j(?:ava)?|ecma)script["']/g
-	  , noSCRIPT_TYPE = 'type="text/noscript+javascript"';
+	var SCRIPT_TYPEs   = /type=["'](?:text|application)\/(?:x-)?(?:j(?:ava)?|ecma)script["']/g
+	  , noSCRIPT_TYPE  = 'type="text/noscript+javascript"'
+	  , noSCRIPT_TYPEs = /type="text\/noscript\+javascript"/g
+	  , SCRIPT_TYPE    = 'type="text/javascript"';
 
-	// disable SCRIPTs...
-	function noSCRIPT( source ) {
+	function disableSCRIPT( source ) {
 		return source.replace( SCRIPT_TYPEs, noSCRIPT_TYPE );
 	}
+	function enableSCRIPT( source ) {
+		return source.replace( noSCRIPT_TYPEs, SCRIPT_TYPE);
+	}
 
-	filter.noSCRIPT = noSCRIPT;
+	filter.disableSCRIPT = disableSCRIPT;
+	filter.enableSCRIPT  = enableSCRIPT;
 
 })(Vizard.Filter);
