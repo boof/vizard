8e343637ac09e14e086e66c1856a20bd4df59111
Florian Aßmann <flazy@fork.de>
2011-07-17 19:33:32 +0200

    Completed input and output functions for base tag filter, fixed doctype dependency.

--- source/filter/base_tag.js
+++ source/filter/base_tag.js
@@ -1,15 +1,26 @@
 (function(filter) {
 
+	function build(vizard) {
+		if ( vizard.is('XHTML') ) {
+			return '<base href="' + vizard.href + '" />';
+		} else {
+			return '<base href="' + vizard.href + '">';
+		}
+	}
+
 	// force assets to be loaded correctly...
 	function insertBASE( source ) {
-		var broken  = source.split('</title>', 2)
-		  , tagBASE = '<base href="' + this.href + '" />'; // FIXME only ' />' if XHTML
+		var broken = source.split('</title>', 2);
 
-		broken[1] = tagBASE + broken[1];
+		broken[1] = build( this ) + broken[1];
 
 		return broken.join('</title>');
 	};
+	function removeBASE( source ) {
+		return source.replace( build( this ), '' );
+	}
 
 	filter.insertBASE = insertBASE;
+	filter.removeBASE = removeBASE;
 
 })(Vizard.Filter);
