33fd36d2b1ab5b3e235ebdc5860035569ef2ef19
Florian Aßmann <flazy@fork.de>
2011-08-03 11:47:28 +0200

    Fixed offset errors for frames with scrollTop on resize.

--- source/ui/frame.js
+++ source/ui/frame.js
@@ -3,14 +3,21 @@
 	var $  = Vizard.jQuery
 	  , UI = Vizard.UI;
 
-	UI.addStyle('.vizard-ui-frame', 'overflow-x: scroll; overflow-y: hidden; position: absolute;');
+	UI.addStyle('.vizard-ui-frame', {
+		'overflow-x': 'scroll',
+		'overflow-y': 'hidden',
+		'position': 'absolute'
+	});
 
 	return UI.frame = function(element) {
 		var $$    = $( element )
 		  , frame = $('<div class="vizard-ui-frame">', element);
 
 		$.window.resize(function() {
-			frame.offset( $$.offset() ).width( $$.width() );
+			// RADAR frame.parent().scrollTop()
+			frame.width( $$.width() ).offset({
+				left: $$.offset().left
+			});
 		});
 
 		return frame.offset( $$.offset() ).width( $$.width() );
